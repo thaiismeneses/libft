74a2611088092a9bead03b1193e7101bf8e19859
Thaís Meneses <87568038+thaiismeneses@users.noreply.github.com>
2024-02-22 20:27:58 -0300

    Update libft.h

--- libft.h
+++ libft.h
@@ -56,6 +56,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	**ft_split(const char *s, char c);
 //bonus
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
